86fe7d2c622e11ab4981cdb264313a18c00198d4
Smoothhyy <zabkarabka506@gmail.com>
2024-03-17 14:56:49 +0100

    dodatkowy rozdzial do pi, pare poprawek

--- scripts/pi.js
+++ scripts/pi.js
@@ -4,13 +4,17 @@ const Obj2 = document.querySelectorAll(".animate-pt2");
 const Obj3 = document.querySelectorAll(".animate-pt3");
 const Obj4 = document.querySelectorAll(".animate-pt4");
 const Obj5 = document.querySelectorAll(".animate-pt5");
-const Interval = 300;
+const Obj6 = document.querySelectorAll(".animate-pt6");
+const Obj7 = document.querySelectorAll(".animate-pt7");
+let interval = 300;
 let yIndex = 2.5;
 let done_obj1 = false;
 let done_obj2 = false;
 let done_obj3 = false;
 let done_obj4 = false;
 let done_obj5 = false;
+let done_obj6 = false;
+let done_obj7 = false;
 
 $(document).ready(function(){   
     if(!done_obj1){
@@ -27,7 +31,7 @@ $(document).ready(function(){
                     "transform",`translateY(${yIndex}px)`
                 );
 
-            }, index * Interval);
+            }, index * interval);
         })
     }
    
@@ -54,7 +58,7 @@ $(".history-of-pi").click(function(){
                 $(element).css(
                     "transform",`translateY(${yIndex}px)`
                 );
-            }, index * Interval);
+            }, index * interval);
         })
     }
     done_obj2 = true;
@@ -75,7 +79,7 @@ $(".history-of-pi").click(function(){
                         "transform",`translateY(${yIndex}px)`
                     );
     
-                }, index * Interval);
+                }, index * interval);
             })
         }
         done_obj3 = true;
@@ -96,7 +100,7 @@ $(".fun-facts-pi-arrow").click(function(){
                     "transform",`translateY(${yIndex}px)`
                 );
 
-            }, index * Interval);
+            }, index * interval);
         })
     }
     done_obj4 = true;
@@ -119,14 +123,69 @@ $(".fun-facts-pi").click(function(){
                     "transform",`translateY(${yIndex}px)`
                 );
 
-            }, index * Interval);
+            }, index * interval);
         })
     }
     done_obj5 = true;
 
+    UpdateTitle("ciekawostki");
     $(".show-more-3").slideDown(500);
+    $(".examples-arrow").delay(5000).slideDown(500, () => {
+        if(!done_obj6){
+            interval = 500;
+            Obj6.forEach(function(element, index){ 
+                setTimeout(function () {
+                    yIndex = yIndex + 2.5
+                    console.log(element);
+                    $(element).css(
+                        "opacity","1"
+                    )
+    
+                    $(element).css(
+                        "transform",`translateY(${yIndex}px)`
+                    );
+    
+                }, index * interval);
+            })
+        }
+        done_obj6 = true;
+    }).delay(2700).queue( function(){
+        if(!done_obj7){
+            interval = 300;
+            Obj7.forEach(function(element, index){ 
+                setTimeout(function () {
+                    yIndex = yIndex + 2.5
+                    console.log(element);
+                    $(element).css(
+                        "opacity","1"
+                    )
+    
+                    $(element).css(
+                        "transform",`translateY(${yIndex}px)`
+                    );
+    
+                }, index * interval);
+            })
+        }
+        done_obj7 = true;
+        $(".animate-ended").delay(1000).slideDown(500);
+    })
+
+
 })
-UpdateTitle("ciekawostki");
+$(".examples-arrow").click(function() {
+    $(".examples-arrow").css(
+        "display", "none"
+    )
+    $(".fun-facts-pi").slideUp(500);
+    $(".show-more-3").slideUp(500);
+    $(".examples").delay(500).slideDown(500);
+})
+$(".examples").click(function() {
+    $(".show-more-4").slideDown(500);
+})
+
+
 })
 
 function UpdateTitle(newTitle){
